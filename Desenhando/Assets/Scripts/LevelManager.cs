9aeff27af934cffd8965ac91e4b1caa4eecfd026
FelipeGiardini14 <felipegiardini2000@outlook.com>
2023-07-25 16:54:27 -0300

    adicionadas as fases restantes no SelectMenu

--- Desenhando/Assets/Scripts/LevelManager.cs
+++ Desenhando/Assets/Scripts/LevelManager.cs
@@ -4,6 +4,54 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
+/*
+public class LevelManager : MonoBehaviour
+{
+    int levelsUnlocked;
+    public Button[] botoes;
+
+    void Start()
+    {
+        levelsUnlocked = PlayerPrefs.GetInt("levelsUnlocked_3", 1);
+        UpdateButtonStates();
+    }
+
+    void UpdateButtonStates()
+    {
+        for (int i = 0; i < botoes.Length; i++)
+        {
+            if (i + 2 > levelsUnlocked)
+            {
+                // Desativar o botão e parar a animação (substitua "avancar" pelo nome do Animator do botão)
+                botoes[i].gameObject.SetActive(false);
+                Animator avancar = botoes[i].GetComponent<Animator>();
+                if (avancar != null)
+                {
+                    avancar.StopPlayback();
+                }
+            }
+            else
+            {
+                // Ativar o botão e reproduzir a animação (substitua "avancar" pelo nome do Animator do botão)
+                botoes[i].gameObject.SetActive(true);
+                Animator avancar = botoes[i].GetComponent<Animator>();
+                if (avancar != null)
+                {
+                    avancar.Play("Avancar");
+                }
+            }
+        }
+    }
+
+    public void UnlockNextLevel()
+    {
+        levelsUnlocked++;
+        PlayerPrefs.SetInt("levelsUnlocked_3", levelsUnlocked);
+        UpdateButtonStates();
+    }
+}
+*/
+
 public class LevelManager : MonoBehaviour
 {
     int levelsUnlocked;
