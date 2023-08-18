fd5795036051267ee26bcb4559b8739be275a04d
FelipeGiardini14 <felipegiardini2000@outlook.com>
2023-08-18 16:29:30 -0300

    Adicionado o botão de fechar o jogo e o botão de fullscreen.

---
+++ Desenhando/Assets/Scripts/FullscreenToggle.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FullscreenToggle : MonoBehaviour
+{
+    private bool isFullscreen = false; // Mantém o estado atual da tela cheia
+    private Button toggleButton;
+
+    public Sprite fullscreen;
+    public Sprite window;
+
+    private Image buttonImage;
+    private bool isFullscreenImage = true;
+
+    private void Start()
+    {
+        toggleButton = GetComponent<Button>();
+        toggleButton.onClick.AddListener(ToggleFullscreen);
+
+        /*
+        buttonImage = GetComponent<Image>();
+        isFullscreenImage = PlayerPrefs.GetInt("ToggleImageState", 1) == 1; // Carrega o estado salvo do PlayerPrefs
+
+        if (isFullscreenImage)
+        {
+            buttonImage.sprite = fullscreen;
+        }
+        else
+        {
+            buttonImage.sprite = window;
+        }
+        */
+
+        buttonImage = GetComponent<Image>();
+        buttonImage.sprite = fullscreen;
+    }
+
+    public void ToggleFullscreen()
+    {
+        isFullscreen = !isFullscreen; // Alterna o estado da tela cheia
+
+        // Alterna entre tela cheia e janela
+        Screen.fullScreen = isFullscreen;
+    }
+    public void ToggleImage()
+    {
+        isFullscreenImage = !isFullscreenImage;
+
+        if (isFullscreenImage)
+        {
+            buttonImage.sprite = fullscreen;
+        }
+        else
+        {
+            buttonImage.sprite = window;
+        }
+
+        /*
+        // Salva o estado do botão no PlayerPrefs
+        PlayerPrefs.SetInt("ToggleImageState", isFullscreenImage ? 1 : 0);
+        */
+    }
+}
